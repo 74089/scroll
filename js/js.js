d20a2a4b65c640431f655f0e5aa99aab92b95643
Stef <25659722+74089@users.noreply.github.com>
2019-09-11 13:28:00 +0200

    Buttons scroll to said color
    Made it so the buttons scroll to the said color but in the most easy but dirty way

--- js/js.js
+++ js/js.js
@@ -1,3 +1,28 @@
 function test() {
   alert("I am a test alert!");
 }
+
+function button1() {
+  var elmnt = document.getElementById("red");
+  elmnt.scrollIntoView();
+}
+function button2() {
+  var elmnt = document.getElementById("orange");
+  elmnt.scrollIntoView();
+}
+function button3() {
+  var elmnt = document.getElementById("yellow");
+  elmnt.scrollIntoView();
+}
+function button4() {
+  var elmnt = document.getElementById("green");
+  elmnt.scrollIntoView();
+}
+function button5() {
+  var elmnt = document.getElementById("blue");
+  elmnt.scrollIntoView();
+}
+function button6() {
+  var elmnt = document.getElementById("purple");
+  elmnt.scrollIntoView();
+}
